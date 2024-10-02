3cac1c2f330c973a67c113b91cdd00b873d3ea0d
goushaa <ahmed.bassem2002@gmail.com>
2024-10-02 16:17:26 +0300

    installation of jenkins and docker script

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_route_table_association" "public_rt_association" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_security_group" "instance_sg" {
+resource "aws_security_group" "k3s_sg" {
   vpc_id = aws_vpc.main_vpc.id
 
   ingress {
@@ -65,7 +65,36 @@ resource "aws_security_group" "instance_sg" {
   }
 
   tags = {
-    Name = "k3s-jenkins-sg"
+    Name = "k3s-sg"
+  }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
   }
 }
 
@@ -73,52 +102,52 @@ variable "ubuntu_ami" {
   default = "ami-0e86e20dae9224db8" # Ubuntu ISO in us-east-1
 }
 
-resource "aws_instance" "k3s" {
-  ami                    = var.ubuntu_ami
-  instance_type         = "t2.micro" 
-  subnet_id             = aws_subnet.public_subnet.id
-  associate_public_ip_address = true
-  security_groups       = [aws_security_group.instance_sg.id]
-  user_data = <<-EOF
-                #!/bin/bash
-                exec > >(tee /var/log/user-data.log) 2>&1
+# resource "aws_instance" "k3s" {
+#   ami                    = var.ubuntu_ami
+#   instance_type         = "t2.micro" 
+#   subnet_id             = aws_subnet.public_subnet.id
+#   associate_public_ip_address = true
+#   security_groups       = [aws_security_group.k3s_sg.id]
+#   user_data = <<-EOF
+#                 #!/bin/bash
+#                 exec > >(tee /var/log/user-data.log) 2>&1
 
-                apt update
-                apt install -y curl
+#                 apt update
+#                 apt install -y curl
 
-                curl -sfL https://get.k3s.io | sh -
-                chmod 644 /etc/rancher/k3s/k3s.yaml
-                chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
+#                 curl -sfL https://get.k3s.io | sh -
+#                 chmod 644 /etc/rancher/k3s/k3s.yaml
+#                 chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
 
-                curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-                chmod 700 get_helm.sh
-                ./get_helm.sh
+#                 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+#                 chmod 700 get_helm.sh
+#                 ./get_helm.sh
 
-                # Set KUBECONFIG environment variable for the current session
-                export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+#                 # Set KUBECONFIG environment variable for the current session
+#                 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-                # Add KUBECONFIG export to the user's bashrc for future sessions
-                echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/ubuntu/.bashrc
+#                 # Add KUBECONFIG export to the user's bashrc for future sessions
+#                 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/ubuntu/.bashrc
 
-                helm repo add bitnami https://charts.bitnami.com/bitnami
-                helm repo update
-                helm install nginx bitnami/nginx
+#                 helm repo add bitnami https://charts.bitnami.com/bitnami
+#                 helm repo update
+#                 helm install nginx bitnami/nginx
 
-                sudo -u ubuntu helm repo add bitnami https://charts.bitnami.com/bitnami
-                sudo -u ubuntu helm repo update
-                EOF
+#                 sudo -u ubuntu helm repo add bitnami https://charts.bitnami.com/bitnami
+#                 sudo -u ubuntu helm repo update
+#                 EOF
 
-  tags = {
-    Name = "k3s"
-  }
-}
+#   tags = {
+#     Name = "k3s"
+#   }
+# }
 
 resource "aws_instance" "k3sbig" {
   ami                    = var.ubuntu_ami
   instance_type         = "t2.small" 
   subnet_id             = aws_subnet.public_subnet.id
   associate_public_ip_address = true
-  security_groups       = [aws_security_group.instance_sg.id]
+  security_groups       = [aws_security_group.k3s_sg.id]
   user_data = <<-EOF
                 #!/bin/bash
                 exec > >(tee /var/log/user-data.log) 2>&1
@@ -159,14 +188,26 @@ resource "aws_instance" "jenkins" {
   instance_type         = "t2.micro" 
   subnet_id             = aws_subnet.public_subnet.id
   associate_public_ip_address = true
-  security_groups       = [aws_security_group.instance_sg.id]
+  security_groups       = [aws_security_group.jenkins_sg.id]
   user_data = <<-EOF
-                #!/bin/bash
-                apt update
-                apt install -y software-properties-common
-                add-apt-repository --yes --update ppa:ansible/ansible
-                apt install -y ansible
-                EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user-data.log) 2>&1
+
+              # Update package lists and install necessary packages
+              apt update
+              apt install -y curl software-properties-common
+
+              # Add Ansible PPA and install Ansible
+              add-apt-repository --yes --update ppa:ansible/ansible
+              apt install -y ansible
+
+              # Download the Ansible playbook for Docker and Jenkins installation
+              curl -O https://raw.githubusercontent.com/goushaa/DEPI-Ansible/refs/heads/main/install_docker_jenkins.yaml
+              ansible-playbook install_docker_jenkins.yaml
+
+              # Add ubuntu user to the docker group
+              usermod -aG docker ubuntu
+              EOF
 
   tags = {
     Name = "jenkins"
